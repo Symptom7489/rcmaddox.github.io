fd8dfb087e3c0257773f1b42169e3bf75532ab3e
rcmaddox <65855506+rcmaddox@users.noreply.github.com>
2022-10-28 11:35:27 -0500

    adding

---
+++ js/about.05c1f0d0.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkstore_vue"]=self["webpackChunkstore_vue"]||[]).push([[443],{7381:function(e,n,u){u.r(n),u.d(n,{default:function(){return l}});var t=u(3396);const r={class:"about"},s=(0,t._)("h1",null,"This is an about page",-1),a=[s];function c(e,n){return(0,t.wg)(),(0,t.iD)("div",r,a)}var o=u(89);const i={},f=(0,o.Z)(i,[["render",c]]);var l=f}}]);
+//# sourceMappingURL=about.05c1f0d0.js.map
